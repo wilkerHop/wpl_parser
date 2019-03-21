4cb244a769c9d618502fdc15b22c02ef52538807
Wilker Ribeiro <rekliw97@gmail.com>
2019-03-21 02:41:17 -0300

    separate the code in methods

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const convert = require('xml-js');
 const fs = require('fs');
 
-(async () => {
-    const xml = await fs.readFileSync('./Cleaning.wpl', 'utf8');
-    const raw = JSON.parse(convert.xml2json(xml, {
-        compact: true,
-        spaces: 4
-    }));
+const WPLsPath = './WPLs';
+const JSONsPath = './JSONs'
 
-    const playlist = {
-        title: raw.smil.head.title._text,
-        tracks_files: raw.smil.body.seq.media.map(e => e._attributes.src.split(/\\|\//).pop()),
-    }
-    await fs.writeFileSync(playlist.title +'.json', JSON.stringify(playlist), {
-        encoding: 'utf-8'
-    })
-})();
+const getWPLs = async () => await fs.readdirSync(WPLsPath);
+getWPLs().then(r => console.log(r));
+
+const readWPLs = async file => JSON.parse(convert.xml2json(await fs.readFileSync(`${WPLsPath}/${file}`, 'utf8'), {
+  compact: true,
+}), );
+
+const formatFileName = name => name.split(/[\\\\/:*?\"<>|]/).join('-')
+
+const writeJSON = async obj => fs.writeFileSync(`${JSONsPath}/${formatFileName(obj.title || obj.smil.head.title._text+ '.raw')}.json`, JSON.stringify(obj));
+
+const createPlaylistObj = wpl => {
+  return {
+    title: wpl.smil.head.title._text,
+    tracks_files: wpl.smil.body.seq.media.map(e =>
+      e._attributes.src.split(/\\|\//).pop(),
+    ),
+  };
+};
+
+(async () =>
+  (await getWPLs()).forEach(async file => {
+    const raw = await readWPLs(file);
+    
+    if (file == 'DubsTroniks.wpl')
+      console.log(createPlaylistObj(raw))
+
+    Promise.all([writeJSON(raw), writeJSON(createPlaylistObj(raw))]);
+  }))
+();
